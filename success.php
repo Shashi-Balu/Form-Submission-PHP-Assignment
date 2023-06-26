06d41d4ebbfaf595b64c14ac1860f238e3e8337b
ShashiBalu <shashibalu8@gmail.com>
2023-06-26 15:53:14 +0530

    Form submission

---
+++ success.php
@@ -0,0 +1,14 @@
+<?php
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+
+// Do further processing with the form data
+
+// Example: Display the submitted data
+echo "First Name: " . $first_name . "<br>";
+echo "Last Name: " . $last_name . "<br>";
+echo "Date of Birth: " . $dob . "<br>";
+echo "Gender: " . $gender . "<br>";
+?>
